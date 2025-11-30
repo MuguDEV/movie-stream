a470031e8ec134de8d18b6d42e155e633d72d5a0
Mugunthan <the.mugunthan@gmail.com>
2025-11-30 21:10:23 +0530

    fix: Remove body parsers to resolve proxy timeout

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Proxy for Seedr
 app.use('/seedr', createProxyMiddleware({
